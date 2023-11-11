c1ea7573631a7ebd93dc43b87b916d73063eea2d
Gina <9094115+ginazampino@users.noreply.github.com>
2023-11-11 14:11:51 -0500

    Add test for event emission

--- src/components/Tests.vue
+++ src/components/Tests.vue
@@ -1,16 +1,19 @@
 <template>
     <AppHeader />
     <RandomNumber />
+    <LoginForm />
 </template>
 
 <script>
     import AppHeader from './AppHeader.vue';
     import RandomNumber from './RandomNumber.vue';
+    import LoginForm from './LoginForm.vue';
 
     export default {
         components: {
             AppHeader,
             RandomNumber,
+            LoginForm,
         },
     };
 </script>
